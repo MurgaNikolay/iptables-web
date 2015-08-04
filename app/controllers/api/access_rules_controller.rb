943f0dd197e7035c16f7334b51dc8b4573ac13ec
Nikolay Murga <nikolay.m@murga.kiev.ua>
2015-08-04 19:22:08 +0300

    Add etag for caching responce

--- app/controllers/api/access_rules_controller.rb
+++ app/controllers/api/access_rules_controller.rb
@@ -1,11 +1,9 @@
 module Api
   class AccessRulesController < Api::BaseController
     def index
-      render json: current_node.full_access_rules, each_serializer: Api::AccessRuleSerializer
-    end
-
-    def show
-      puts params
+      full_access_rules = current_node.full_access_rules
+      response.headers['Etag'] = Digest::MD5.hexdigest(full_access_rules.to_json)
+      render json: full_access_rules, each_serializer: Api::AccessRuleSerializer
     end
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,18 @@
+options = {
+  scope: 'email, profile'
+}
+
+unless Rails.env.production?
+  options.merge!({
+      client_options: {
+        ssl: {
+          ca_file: '/usr/local/etc/openssl/certs/ca-bundle.crt',
+          ca_path: '/usr/local/etc/openssl/certs'
+        }
+      }
+    })
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Settings.google_client_id, Settings.google_client_secret
-  {
-    :scope => "email, profile"
-  }
+  provider(:google_oauth2, Settings.google_client_id, Settings.google_client_secret, options)
 end
